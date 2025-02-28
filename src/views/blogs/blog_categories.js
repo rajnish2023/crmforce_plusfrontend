559b550bbfe2a3ca3b6274ec97500c0e3e2f3f81
rajnish2023 <rajnish.kumar@dynamicssquare.com>
2025-02-28 15:07:16 +0530

    add category image in category list

--- src/views/blogs/blog_categories.js
+++ src/views/blogs/blog_categories.js
@@ -37,7 +37,8 @@ const BlogCategory = () => {
   const [newCategory, setNewCategory] = useState({
     title: '',
     slug: '',
-    status: 'Active'
+    status: 'Active',
+    image: null
   });
   const [errors, setErrors] = useState({
     title: '',
@@ -51,6 +52,8 @@ const BlogCategory = () => {
 
   // State for editing category
   const [editCategory, setEditCategory] = useState(null);
+
+  const APP_URL = 'https://crmfoceplus-backend.onrender.com';
 
   const loadCategories = async () => {
     try {
@@ -77,14 +80,18 @@ const BlogCategory = () => {
   const toggleModal = () => {
     setModalVisible(!modalVisible);
     if (modalVisible) {
-      setNewCategory({ title: '', slug: '', status: 'Active' });  // Reset form when closing
+      setNewCategory({ title: '', slug: '', status: 'Active', image: null });  // Reset form when closing
       setServerError(''); // Clear error when modal closes
     }
   };
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewCategory((prevCategory) => ({ ...prevCategory, [name]: value }));
+    const { name, value, files } = e.target;
+    if (name === 'image') {
+      setNewCategory((prevCategory) => ({ ...prevCategory, image: files[0] }));
+    } else {
+      setNewCategory((prevCategory) => ({ ...prevCategory, [name]: value }));
+    }
   };
 
   const validateForm = () => {
@@ -109,15 +116,24 @@ const BlogCategory = () => {
     if (!validateForm()) {
       return;
     }
-   setLoading(true);
+    setLoading(true);
     try {
-      const response = await createCategory(newCategory);
+      const formData = new FormData();
+      formData.append('title', newCategory.title);
+      formData.append('slug', newCategory.slug);
+      formData.append('status', newCategory.status);
+      if (newCategory.image) {
+        formData.append('image', newCategory.image);
+      }
+
+      const response = await createCategory(formData);
       loadCategories();  
 
       setNewCategory({
         title: '',
         slug: '',
-        status: 'Active'
+        status: 'Active',
+        image: null
       });
       setLoading(false);
       setModalVisible(false);
@@ -125,7 +141,6 @@ const BlogCategory = () => {
     } catch (error) {
       console.error('Error creating category:', error);
       if (error.response && error.response.data) {
-        
         setServerError(error.response.data.message || 'An error occurred while creating the category.');
       } else {
         setServerError('An internal server error occurred.');
@@ -185,7 +200,8 @@ const BlogCategory = () => {
     setNewCategory({
       title: category.title,
       slug: category.slug,
-      status: category.status
+      status: category.status,
+      image: null
     });
     setModalVisible(true);
   };
@@ -196,11 +212,19 @@ const BlogCategory = () => {
     }
 
     try {
-      await updateCategory(editCategory._id, newCategory);
+      const formData = new FormData();
+      formData.append('title', newCategory.title);
+      formData.append('slug', newCategory.slug);
+      formData.append('status', newCategory.status);
+      if (newCategory.image) {
+        formData.append('image', newCategory.image);
+      }
+
+      await updateCategory(editCategory._id, formData);
       loadCategories(); // Reload categories after update
 
       setEditCategory(null);
-      setNewCategory({ title: '', slug: '', status: 'Active' });
+      setNewCategory({ title: '', slug: '', status: 'Active', image: null });
       setModalVisible(false);
       setServerError('');  // Clear any previous server errors
     } catch (error) {
@@ -222,128 +246,141 @@ const BlogCategory = () => {
   return (
     <>
       {loading && (
-                                 <div className="loading-overlay">
-                                     <div className="loading-content">
-                                         <CSpinner color="primary" size="lg" />
-                                         <p>Please wait, Your request is processing...</p>
-                                     </div>
-                                 </div>
-                             )}
-    <CRow>
-      <CCol xs={6} className="d-flex justify-content-start">
-        <CButton color="primary" onClick={() => setModalVisible(true)}>Create Category</CButton>
-      </CCol>
-      <CCol xs={6} className="d-flex justify-content-end">
-        <CInputGroup>
-          <CFormInput
-            placeholder="Search category..."
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-        </CInputGroup>
-      </CCol>
+        <div className="loading-overlay">
+          <div className="loading-content">
+            <CSpinner color="primary" size="lg" />
+            <p>Please wait, Your request is processing...</p>
+          </div>
+        </div>
+      )}
+      <CRow>
+        <CCol xs={6} className="d-flex justify-content-start">
+          <CButton color="primary" onClick={() => setModalVisible(true)}>Create Category</CButton>
+        </CCol>
+        <CCol xs={6} className="d-flex justify-content-end">
+          <CInputGroup>
+            <CFormInput
+              placeholder="Search category..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+          </CInputGroup>
+        </CCol>
 
-      <CCol xs={12} className="mt-4">
-        <CTable striped hover>
-          <CTableHead>
-            <CTableRow>
-              <CTableHeaderCell scope="col" className="py-3 px-4">#</CTableHeaderCell>
-              <CTableHeaderCell scope="col" className="py-3 px-4">Title</CTableHeaderCell>
-              <CTableHeaderCell scope="col" className="py-3 px-4">Slug</CTableHeaderCell>
-              <CTableHeaderCell scope="col" className="py-3 px-4">Status</CTableHeaderCell>
-              <CTableHeaderCell scope="col" className="py-3 px-4">Actions</CTableHeaderCell>
-            </CTableRow>
-          </CTableHead>
-          <CTableBody>
-            {filteredCategories.map((category, index) => (
-              <CTableRow key={category._id}>
-                <CTableHeaderCell scope="row" className="py-3 px-4">{index + 1}</CTableHeaderCell>
-                <CTableDataCell className="py-3 px-4">{category.title}</CTableDataCell>
-                <CTableDataCell className="py-3 px-4">{category.slug}</CTableDataCell>
-                <CTableDataCell className="py-3 px-4">{category.status}</CTableDataCell>
-                <CTableDataCell className="py-3 px-4">
-                  <CButton className="mx-3" color="info" onClick={() => handleCategoryEdit(category)}>Edit</CButton>
-                  <CButton color="danger" onClick={() => toggleDeleteModal(category)}>Delete</CButton>
-                </CTableDataCell>
+        <CCol xs={12} className="mt-4">
+          <CTable striped hover>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col" className="py-3 px-4">#</CTableHeaderCell>
+                <CTableHeaderCell scope="col" className="py-3 px-4">Title</CTableHeaderCell>
+                <CTableHeaderCell scope="col" className="py-3 px-4">Slug</CTableHeaderCell>
+                <CTableHeaderCell scope="col" className="py-3 px-4">Status</CTableHeaderCell>
+                <CTableHeaderCell scope="col" className="py-3 px-4">Actions</CTableHeaderCell>
               </CTableRow>
-            ))}
-          </CTableBody>
-        </CTable>
-      </CCol>
+            </CTableHead>
+            <CTableBody>
+              {filteredCategories.map((category, index) => (
+                <CTableRow key={category._id}>
+                  <CTableHeaderCell scope="row" className="py-3 px-4">{index + 1}</CTableHeaderCell>
+                  <CTableDataCell className="py-3 px-4">{category.title}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4">{category.slug}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4">{category.status}</CTableDataCell>
+                  <CTableDataCell className="py-3 px-4">
+                    <CButton className="mx-3" color="info" onClick={() => handleCategoryEdit(category)}>Edit</CButton>
+                    <CButton color="danger" onClick={() => toggleDeleteModal(category)}>Delete</CButton>
+                  </CTableDataCell>
+                </CTableRow>
+              ))}
+            </CTableBody>
+          </CTable>
+        </CCol>
 
-      {/* Delete Confirmation Modal */}
-      <CModal visible={deleteModalVisible} onClose={handleDeleteCancel}>
-        <CModalHeader>
-          <CModalTitle>Confirm Deletion</CModalTitle>
-        </CModalHeader>
-        <CModalBody>
-          <p>Are you sure you want to delete this category?</p>
-        </CModalBody>
-        <CModalFooter>
-          <CButton color="secondary" onClick={handleDeleteCancel}>Cancel</CButton>
-          <CButton color="danger" onClick={handleDeleteConfirmation}>Delete</CButton>
-        </CModalFooter>
-      </CModal>
+        {/* Delete Confirmation Modal */}
+        <CModal visible={deleteModalVisible} onClose={handleDeleteCancel}>
+          <CModalHeader>
+            <CModalTitle>Confirm Deletion</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <p>Are you sure you want to delete this category?</p>
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="secondary" onClick={handleDeleteCancel}>Cancel</CButton>
+            <CButton color="danger" onClick={handleDeleteConfirmation}>Delete</CButton>
+          </CModalFooter>
+        </CModal>
 
-      {/* Category Create / Update Modal */}
-      <CModal visible={modalVisible} onClose={() => setModalVisible(false)} size="lg">
-        <CModalHeader>
-          <CModalTitle>{editCategory ? 'Edit Category' : 'Create New Category'}</CModalTitle>
-        </CModalHeader>
-        <CModalBody>
-          <CForm>
-            {serverError && <div className="text-danger mb-3">{serverError}</div>}  {/* Display the server error */}
+        {/* Category Create / Update Modal */}
+        <CModal visible={modalVisible} onClose={() => setModalVisible(false)} size="lg">
+          <CModalHeader>
+            <CModalTitle>{editCategory ? 'Edit Category' : 'Create New Category'}</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <CForm>
+              {serverError && <div className="text-danger mb-3">{serverError}</div>}  {/* Display the server error */}
 
-            <CRow>
-              <CCol xs={6} className="mb-3">
-                <CFormLabel htmlFor="categoryTitle">Category Title</CFormLabel>
-                <CFormInputModal
-                  id="categoryTitle"
-                  name="title"
-                  placeholder="Enter category title"
-                  value={newCategory.title}
-                  onChange={handleTitleChange}
+              <CRow>
+                <CCol xs={6} className="mb-3">
+                  <CFormLabel htmlFor="categoryTitle">Category Title</CFormLabel>
+                  <CFormInputModal
+                    id="categoryTitle"
+                    name="title"
+                    placeholder="Enter category title"
+                    value={newCategory.title}
+                    onChange={handleTitleChange}
+                  />
+                  {errors.title && <CFormText className="text-danger">{errors.title}</CFormText>}
+                </CCol>
+                <CCol xs={6} className="mb-3">
+                  <CFormLabel htmlFor="categorySlug">Slug</CFormLabel>
+                  <CFormInputModal
+                    id="categorySlug"
+                    name="slug"
+                    placeholder="Enter category slug"
+                    value={newCategory.slug}
+                    onChange={handleSlugChange}
+                  />
+                  {errors.slug && <CFormText className="text-danger">{errors.slug}</CFormText>}
+                </CCol>
+              </CRow>
+              <CRow>
+                <CCol xs={6} className="mb-3">
+                  <CFormLabel htmlFor="categoryImage">Upload Category Image</CFormLabel>
+                  <CFormInputModal
+                    id="categoryImage"
+                    name="image"
+                    type="file"
+                    onChange={handleInputChange}
+                  />
+                </CCol>
+                <img
+                 src={`${APP_URL}/uploads/${newCategory.categoryimg}`}
+                  alt="Category Banner Preview"
+                  style={{ width: '10%', height: 'auto', borderRadius: '8px' }}
                 />
-                {errors.title && <CFormText className="text-danger">{errors.title}</CFormText>}
-              </CCol>
-              <CCol xs={6} className="mb-3">
-                <CFormLabel htmlFor="categorySlug">Slug</CFormLabel>
-                <CFormInputModal
-                  id="categorySlug"
-                  name="slug"
-                  placeholder="Enter category slug"
-                  value={newCategory.slug}
-                  onChange={handleSlugChange}
-                />
-                {errors.slug && <CFormText className="text-danger">{errors.slug}</CFormText>}
-              </CCol>
-            </CRow>
-            <CRow>
-              <CCol xs={6} className="mb-3">
-                <CFormLabel htmlFor="categoryStatus">Status</CFormLabel>
-                <CFormSelect
-                  id="categoryStatus"
-                  name="status"
-                  value={newCategory.status}
-                  onChange={handleInputChange}
-                >
-                  <option value="Active">Active</option>
-                  <option value="Inactive">Inactive</option>
-                </CFormSelect>
-              </CCol>
-            </CRow>
-          </CForm>
-        </CModalBody>
-        <CModalFooter>
-          <CButton color="secondary" onClick={() => setModalVisible(false)}>Cancel</CButton>
-          <CButton color="primary" onClick={editCategory ? handleCategoryUpdate : handleCategoryCreate}>
-            {editCategory ? 'Update' : 'Create'}
-          </CButton>
-        </CModalFooter>
-      </CModal>
-    </CRow>
+                <CCol xs={6} className="mb-3">
+                  <CFormLabel htmlFor="categoryStatus">Status</CFormLabel>
+                  <CFormSelect
+                    id="categoryStatus"
+                    name="status"
+                    value={newCategory.status}
+                    onChange={handleInputChange}
+                  >
+                    <option value="Active">Active</option>
+                    <option value="Inactive">Inactive</option>
+                  </CFormSelect>
+                </CCol>
+              </CRow>
+            </CForm>
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="secondary" onClick={() => setModalVisible(false)}>Cancel</CButton>
+            <CButton color="primary" onClick={editCategory ? handleCategoryUpdate : handleCategoryCreate}>
+              {editCategory ? 'Update' : 'Create'}
+            </CButton>
+          </CModalFooter>
+        </CModal>
+      </CRow>
     </>
-    
   );
 };
 
